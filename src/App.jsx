60fa74f64c76db1b1f7afa884c5056e44cecac01
vibhorsoni19 <vibhor10soni1999@gmail.com>
2025-03-29 17:00:58 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <p className="text-3xl font-bold text-green-500">hello</p>
+    </>
+  )
+}
+
+export default App
